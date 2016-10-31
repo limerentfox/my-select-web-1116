34e65be432cc71fdb52af48bfb0fa40569904cfa
limerentfox <christi@limerentfox.com>
2016-10-31 06:42:01 -0400

    my-select lab complete

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,13 @@
 def my_select(collection)
- # your code here!
+ arr = []
+ arr2 = []
+ i = 0
+ while i < collection.length
+  arr << yield(collection[i])
+  if arr[i] == true
+    arr2 << collection[i]
+  end
+  i = i + 1
+  end
+  arr2
 end
